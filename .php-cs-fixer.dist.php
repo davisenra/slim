1761b3adb0c72d8752a6c7f95c8f7dcf1676a4e9
Davi <davi@backstagedigital.com.br>
2024-03-21 13:59:30 -0300

    migrate from laravel/pint to php-cs-fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,16 @@
+<?php
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12' => true,
+    ])
+    ->setFinder(
+        (new PhpCsFixer\Finder())
+        ->in([
+            __DIR__.'/src',
+            __DIR__.'/config',
+            __DIR__.'/bootstrap',
+            __DIR__.'/public',
+            __DIR__.'/tests',
+        ])
+    );
